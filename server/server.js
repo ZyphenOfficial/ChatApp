939dc582e2e7852d207b863996ef2728fdb95e24
ZyphenCodes <undefeatable234@gmail.com>
2017-12-08 23:40:57 -0500

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
-var server = http.createServer(app)
+var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
@@ -15,21 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from : 'Person',
-    text: 'Potato',
-    createdAt: 123123
-  });
-
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-  })
-
-  socket.on('createEmail', (newEmail) => {
-    console.log('createEmail', newEmail);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
-  socket.on('disconnect', (socket) => {
+  socket.on('disconnect', () => {
     console.log('User has disconnected');
   });
 });
