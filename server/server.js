db300eedff8a04d88c323c7fcf297bb2a2105ba8
ZyphenCodes <undefeatable234@gmail.com>
2017-12-08 23:01:11 -0500

    Message Events - newMessage and createMessage

--- server/server.js
+++ server/server.js
@@ -1,16 +1,40 @@
 const path = require('path');
-
+const http =  require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app)
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.emit('newMessage', {
+    from : 'Person',
+    text: 'Potato',
+    createdAt: 123123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  })
+
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+  });
+
+  socket.on('disconnect', (socket) => {
+    console.log('User has disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
 
